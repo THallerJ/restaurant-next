88411cf97c99e856de572cde367d41ccb835c9ee
Thomas Haller <hallert60@gmail.com>
2023-08-16 18:14:44 -0500

    started making menuitems scale to screen sizes

--- src/app/order/components/MenuItem.tsx
+++ src/app/order/components/MenuItem.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const MenuItem = ({ item }: { item: menuItem }) => {
   return (
-    <div className="flex w-64 flex-col justify-between gap-3 rounded-xl bg-white p-3 shadow-md">
+    <div className="flex w-64 flex-col justify-between gap-3 rounded-xl bg-white p-3 shadow-md xs:w-48 md:w-64">
       {item.image ? (
         <Image src={item.image} alt={item.name} className="rounded-lg" />
       ) : null}
