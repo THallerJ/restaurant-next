727c985cbe1823949fe12a700903959e81577632
Thomas Haller <hallert60@gmail.com>
2023-08-16 17:38:53 -0500

    changed layout

--- src/app/order/components/MenuItem.tsx
+++ src/app/order/components/MenuItem.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const MenuItem = ({ item }: { item: menuItem }) => {
   return (
-    <div className="flex  w-64 flex-col justify-between gap-3 rounded-xl bg-white p-3 shadow-md">
+    <div className="flex w-64 flex-col justify-between gap-3 rounded-xl bg-white p-3 shadow-md">
       {item.image ? (
         <Image src={item.image} alt={item.name} className="rounded-lg" />
       ) : null}
