360d5fdb595620548d202354de3f0f1ace8fd78e
Thomas Haller <hallert60@gmail.com>
2023-08-15 20:54:53 -0500

    added images

--- src/app/order/components/MenuItem.tsx
+++ src/app/order/components/MenuItem.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { menuItem } from "@/types";
+import Image from "next/image";
 
 const MenuItem = ({ item }: { item: menuItem }) => {
   return (
-    <div className="flex h-48 w-48 flex-col justify-end rounded-lg bg-third p-2 shadow-md">
+    <div className="flex h-64 w-64 flex-col justify-end rounded-lg bg-second p-2 shadow-md">
+      {item.image ? <Image src={item.image} alt={item.name} /> : null}
       <span className="font-semibold text-dark">{item.name}</span>
       <span className="text-sm text-dark">{item.details}</span>
     </div>
