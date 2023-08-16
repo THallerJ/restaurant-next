e4f7247eff67e03edcd62f1179eed6a02a080983
Thomas Haller <hallert60@gmail.com>
2023-08-15 20:58:40 -0500

    modified order color thing

--- src/app/order/components/MenuItem.tsx
+++ src/app/order/components/MenuItem.tsx
@@ -4,10 +4,14 @@ import Image from "next/image";
 
 const MenuItem = ({ item }: { item: menuItem }) => {
   return (
-    <div className="flex h-64 w-64 flex-col justify-end rounded-lg bg-second p-2 shadow-md">
-      {item.image ? <Image src={item.image} alt={item.name} /> : null}
-      <span className="font-semibold text-dark">{item.name}</span>
-      <span className="text-sm text-dark">{item.details}</span>
+    <div className="flex h-64 w-64 flex-col justify-between rounded-lg bg-white p-2 shadow-md">
+      {item.image ? (
+        <Image src={item.image} alt={item.name} className="rounded-lg" />
+      ) : null}
+      <div>
+        <span className="font-semibold text-dark">{item.name}</span>
+        <p className="text-sm text-dark">{item.details}</p>
+      </div>
     </div>
   );
 };
