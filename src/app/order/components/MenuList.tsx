5f3762e97849ccc2b31cac8ce44bed1dda11a41b
Thomas Haller <hallert60@gmail.com>
2023-08-16 20:49:56 -0500

    centered title

--- src/app/order/components/MenuList.tsx
+++ src/app/order/components/MenuList.tsx
@@ -3,8 +3,8 @@ import MenuItem from "./MenuItem";
 
 const MenuList = ({ title, items }: { title: string; items: menuItem[] }) => {
   return (
-    <div className="flex flex-col">
-      <h2 className="heading-sm">{title}</h2>
+    <div className="flex flex-col gap-1">
+      <h2 className="heading-sm text-center xxl:text-start">{title}</h2>
       <div className="flex flex-row flex-wrap justify-center gap-4 xxl:justify-start">
         {items.map((item, i) => (
           <MenuItem item={item} key={item.name + i} />
