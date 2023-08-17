99bda334193e99cdfcc1383082c2ddc9fdb0c6e1
Thomas Haller <hallert60@gmail.com>
2023-08-16 21:03:13 -0500

    modified ui of MenuItem

--- src/app/order/components/MenuItem.tsx
+++ src/app/order/components/MenuItem.tsx
@@ -8,8 +8,10 @@ const MenuItem = ({ item }: { item: menuItem }) => {
         <Image src={item.image} alt={item.name} className="rounded-lg" />
       ) : null}
       <div className="flex h-full flex-col justify-between">
-        <span className="font-semibold text-dark">{item.name}</span>
-        <p className="text-xs text-dark">{item.details}</p>
+        <div>
+          <span className="font-semibold text-dark">{item.name}</span>
+          <p className="text-xs text-dark">{item.details}</p>
+        </div>
         <button className="btn mt-3">Add to cart</button>
       </div>
     </div>
