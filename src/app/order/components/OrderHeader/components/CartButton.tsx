d3f3087c6d2e85027cfae61e79f715905430b478
Thomas Haller <hallert60@gmail.com>
2023-08-19 19:46:37 -0500

    started doing cart menu thing

--- src/app/order/components/OrderHeader/components/CartButton.tsx
+++ src/app/order/components/OrderHeader/components/CartButton.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import { Cart } from "@/assets";
 import { useOrder } from "@/app/order/contexts/OrderContext";
+import ClickAway from "@/wrappers/ClickAway";
 
 const CartButton = () => {
   const { cartItems } = useOrder();
+  const [open, setOpen] = useState(false);
 
   return (
-    <button className="relative">
+    <button className="relative" onClick={() => setOpen((prev) => !prev)}>
       <Cart className="h-8 w-8" />
       {cartItems.total > 0 ? (
-        <div className="absolute left-5 top-5 flex h-6 w-6 items-center justify-center rounded-full bg-dark">
+        <div className="absolute bottom-2 left-4 flex h-5 w-5 items-center justify-center rounded-full bg-dark">
           <span className="text-sm font-semibold text-offwhite">
             {cartItems.total}
           </span>
         </div>
       ) : null}
+      <ClickAway callback={() => setOpen(false)}>
+        {open ? (
+          <div className="absolute right-0 top-full h-48 min-w-full rounded-lg bg-dark p-4 text-white">
+            <span>sdsada as a</span>
+          </div>
+        ) : null}
+      </ClickAway>
     </button>
   );
 };
