d0e8fcb2ae0b30560e7a3c538df9cdc05a8e1de0
Thomas Haller <hallert60@gmail.com>
2023-08-19 20:03:04 -0500

    fixed click behavior of cart button

--- src/app/order/components/OrderHeader/components/CartButton.tsx
+++ src/app/order/components/OrderHeader/components/CartButton.tsx
@@ -8,23 +8,25 @@ const CartButton = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <button className="relative" onClick={() => setOpen((prev) => !prev)}>
-      <Cart className="h-8 w-8" />
-      {cartItems.total > 0 ? (
-        <div className="absolute bottom-2 left-4 flex h-5 w-5 items-center justify-center rounded-full bg-dark">
-          <span className="text-sm font-semibold text-offwhite">
-            {cartItems.total}
-          </span>
-        </div>
-      ) : null}
-      <ClickAway callback={() => setOpen(false)}>
+    <ClickAway callback={() => setOpen(false)}>
+      <div className="relative">
+        <button onClick={() => setOpen((prev) => !prev)}>
+          <Cart className="h-8 w-8" />
+          {cartItems.total > 0 ? (
+            <div className="absolute bottom-2 left-4 flex h-5 w-5 items-center justify-center rounded-full bg-dark">
+              <span className="text-sm font-semibold text-offwhite">
+                {cartItems.total}
+              </span>
+            </div>
+          ) : null}
+        </button>
         {open ? (
           <div className="absolute right-0 top-full h-48 min-w-full rounded-lg bg-dark p-4 text-white">
             <span>sdsada as a</span>
           </div>
         ) : null}
-      </ClickAway>
-    </button>
+      </div>
+    </ClickAway>
   );
 };
 
