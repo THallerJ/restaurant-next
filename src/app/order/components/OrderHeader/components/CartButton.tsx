ad8a1f5b183f331e3f65c565368838dea12db8a2
Thomas Haller <hallert60@gmail.com>
2023-08-23 19:30:22 -0500

    difosdifiosd dlslkdsf

--- src/app/order/components/OrderHeader/components/CartButton.tsx
+++ src/app/order/components/OrderHeader/components/CartButton.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import { Cart } from "@/assets";
 import { useOrder } from "@/app/order/contexts/OrderContext";
 import CartContent from "./CartContent";
 
 const CartButton = () => {
   const { cartItems } = useOrder();
-  const [open, setOpen] = useState(false);
 
   return (
-    <div className="relative">
-      <button onClick={() => setOpen((prev) => !prev)}>
+    <div className="group peer relative">
+      <button className="peer">
         <Cart className="h-8 w-8" />
         {cartItems.count > 0 ? (
           <div className="absolute bottom-2 left-4 flex h-5 w-5 items-center justify-center rounded-full bg-dark">
@@ -19,17 +17,12 @@ const CartButton = () => {
           </div>
         ) : null}
       </button>
-      {open ? (
-        <>
-          <div
-            className="fixed left-0 top-0 h-full w-full bg-dark/50"
-            onClick={() => setOpen(false)}
-          />
-          <div className="absolute right-0 top-full h-96 w-80 rounded-lg border-2 border-dark bg-white text-dark shadow-lg">
-            <CartContent />
-          </div>
-        </>
-      ) : null}
+      <div
+        className="absolute right-0 top-full h-96 w-80 scale-0 rounded-lg border-2 border-dark
+          bg-white text-dark shadow-lg group-hover:scale-100"
+      >
+        <CartContent />
+      </div>
     </div>
   );
 };
