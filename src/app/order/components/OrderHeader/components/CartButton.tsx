378c3b817877613438f2a7e6f1967c7fef496bf9
Thomas Haller <hallert60@gmail.com>
2023-08-25 18:59:18 -0500

    did thingskls dlfldsfds sd dsd

--- src/app/order/components/OrderHeader/components/CartButton.tsx
+++ src/app/order/components/OrderHeader/components/CartButton.tsx
@@ -9,9 +9,12 @@ const CartButton = () => {
     <>
       <div className="group peer relative z-[5]">
         <button>
-          <Cart className="h-8 w-8" />
+          <Cart className="h-7 w-7" />
           {cartItems.count > 0 ? (
-            <div className="absolute bottom-2 left-4 flex h-5 w-5 items-center justify-center rounded-full bg-dark">
+            <div
+              className="absolute bottom-2 left-4 flex h-5 w-5 items-center justify-center 
+                rounded-full bg-dark"
+            >
               <span className="text-sm font-semibold text-offwhite">
                 {cartItems.count}
               </span>
@@ -19,13 +22,16 @@ const CartButton = () => {
           ) : null}
         </button>
         <div
-          className="absolute right-0 top-full h-96 w-80 scale-0 rounded-lg border-2 border-dark bg-white
-          text-dark shadow-lg group-focus-within:scale-100 group-hover:scale-100"
+          className="absolute right-0 top-full h-96 w-80 scale-0 rounded-lg border-2 border-dark 
+          bg-white text-dark shadow-lg group-focus-within:scale-100 group-hover:scale-100"
         >
           <CartContent />
         </div>
       </div>
-      <div className="fixed right-0 top-0 h-full w-full scale-0 bg-dark/50 peer-focus-within:scale-100 peer-hover:scale-100" />
+      <div
+        className="fixed right-0 top-0 h-full w-full scale-0 bg-dark/50 peer-focus-within:scale-100 
+          peer-hover:scale-100"
+      />
     </>
   );
 };
