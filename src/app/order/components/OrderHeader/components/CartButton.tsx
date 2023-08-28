ea5e25bb049327146a2f48c897d576bd0c3c3d33
Thomas Haller <hallert60@gmail.com>
2023-08-28 17:32:08 -0500

    fixed cart behavior on mobile

--- src/app/order/components/OrderHeader/components/CartButton.tsx
+++ src/app/order/components/OrderHeader/components/CartButton.tsx
@@ -1,14 +1,21 @@
 import { Cart } from "@/assets";
 import { useOrder } from "@/app/order/contexts/OrderContext";
 import CartContent from "./CartContent";
+import { useState } from "react";
 
 const CartButton = () => {
   const { cartItems } = useOrder();
+  const [hover, setHover] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   return (
     <>
-      <div className="group peer relative z-[5]">
-        <button>
+      <div
+        className="group peer relative z-[5]"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <button onClick={() => setClicked((prev) => !prev)}>
           <Cart className="h-7 w-7" />
           {cartItems.count > 0 ? (
             <div
@@ -22,15 +29,19 @@ const CartButton = () => {
           ) : null}
         </button>
         <div
-          className="absolute right-0 top-full h-96 w-80 scale-0 rounded-lg border-2 border-dark 
-          bg-white text-dark shadow-lg group-focus-within:scale-100 group-hover:scale-100"
+          className={`absolute right-0 top-full h-96 w-80 scale-0 rounded-lg border-2 border-dark 
+            bg-white text-dark shadow-lg ${
+              clicked || hover ? "scale-100" : null
+            }`}
         >
           <CartContent />
         </div>
       </div>
       <div
-        className="fixed right-0 top-0 h-full w-full scale-0 bg-dark/50 peer-focus-within:scale-100 
-          peer-hover:scale-100"
+        onClick={() => setClicked(false)}
+        className={`fixed right-0 top-0 h-full w-full scale-0 bg-dark/50 ${
+          clicked || hover ? "scale-100" : null
+        }`}
       />
     </>
   );
