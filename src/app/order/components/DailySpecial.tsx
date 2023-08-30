a1d0c83a981ab6693635dfcf290d2acc24d305f4
Thomas Haller <hallert60@gmail.com>
2023-08-29 20:51:17 -0500

    added cartdispatch to dailyspeica

---
+++ src/app/order/components/DailySpecial.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { dailySpecial } from "@/constants";
+import Image from "next/image";
+import { useOrder } from "../contexts/OrderContext";
+
+const DailySpecial = () => {
+  const { cartDispatch } = useOrder();
+
+  return (
+    <div className="hidden flex-col gap-1 lg:flex">
+      <h2 className="heading-sm">Today Daily Special</h2>
+      <div
+        className="flex flex-col items-center gap-4 rounded-2xl border-2 border-dark p-4
+        sm:flex-row sm:items-start"
+      >
+        {dailySpecial.image ? (
+          <Image
+            src={dailySpecial.image}
+            className="w-44 rounded-lg"
+            alt="daily-special"
+          />
+        ) : null}
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between">
+            <h4 className="text-xl font-semibold text-dark">
+              {dailySpecial.name}
+            </h4>
+            <span className="font-semibold text-dark">{`$${dailySpecial.price}`}</span>
+          </div>
+          <p>{dailySpecial.details}</p>
+          <button
+            className="btn mt-4 w-48 self-end"
+            onClick={() => cartDispatch({ type: "add", payload: dailySpecial })}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DailySpecial;
--- src/app/order/components/Order.tsx
+++ src/app/order/components/Order.tsx
@@ -5,31 +5,13 @@ import {
   lunchItems,
   dinnerItems,
   drinkItems,
-  dailySpecial,
 } from "@/constants";
-import Image from "next/image";
+import DailySpecial from "./DailySpecial";
 
 const Order = () => {
   return (
     <div className="flex w-full flex-col gap-8">
-      <div className=" hidden flex-col gap-1 lg:flex">
-        <h2 className="heading-sm">Today Daily Special</h2>
-        <div className="flex flex-col items-center gap-4 rounded-2xl border-2 border-dark p-4 sm:flex-row sm:items-start">
-          {dailySpecial.image ? (
-            <Image
-              src={dailySpecial.image}
-              className="w-44 rounded-lg"
-              alt="daily-special"
-            />
-          ) : null}
-          <div className="flex flex-col gap-1 ">
-            <h4 className="text-xl font-semibold text-dark">
-              {dailySpecial.name}
-            </h4>
-            <p className="text-base">{dailySpecial.details}</p>
-          </div>
-        </div>
-      </div>
+      <DailySpecial />
       <OrderList items={starterItems} title="Starters" />
       <OrderList items={breakfastItems} title="Breakfast" />
       <OrderList items={lunchItems} title="Lunch" />
