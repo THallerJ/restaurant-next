52d8ce180f910123dd924096dc80fc19f33e1b58
Thomas Haller <hallert60@gmail.com>
2023-10-02 19:16:34 -0500

    discount code progress again for real

--- src/app/order/submit/components/DiscountCode.tsx
+++ src/app/order/submit/components/DiscountCode.tsx
@@ -3,16 +3,17 @@ import { InputLabel, AnimatedButton } from "@/components";
 const DiscountCode = () => {
   return (
     <div className="flex w-full justify-center sm:px-4">
-      <div className="flex w-full flex-col items-center sm:w-5/6 lg:w-2/3 ">
+      <div className="flex w-full flex-col items-center sm:w-5/6 lg:w-2/3">
         <InputLabel
           id="discount_code"
-          className="input"
-          format="#####"
+          className="input text-2xl tracking-widest"
+          format="5%%%%"
           mask="_"
           label="Discount Code"
           placeholder="_____"
+          patternChar="%"
         />
-        <AnimatedButton className="mt-2 " fullSize>
+        <AnimatedButton className="mt-2" fullSize>
           Apply code
         </AnimatedButton>
       </div>
