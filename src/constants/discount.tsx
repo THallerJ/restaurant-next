4ad5c9970e6301bb734d1050b20550c3a0505a7f
Thomas Haller <hallert60@gmail.com>
2023-10-05 18:22:02 -0500

    added more discount codes

--- src/constants/discount.tsx
+++ src/constants/discount.tsx
@@ -1,5 +1,9 @@
 type discountCode = { code: string; percent: number };
 
-const discountCodes: discountCode[] = [{ code: "00000", percent: 20 }];
+const discountCodes: discountCode[] = [
+  { code: "00000", percent: 20 },
+  { code: "11111", percent: 50 },
+  { code: "22222", percent: 90 },
+];
 
 export default discountCodes;
