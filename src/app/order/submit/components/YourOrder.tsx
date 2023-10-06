2e8817304e90e474a347b5bae284c9938f05417e
Thomas Haller <hallert60@gmail.com>
2023-10-05 19:34:04 -0500

    dididsf

--- src/app/order/submit/components/YourOrder.tsx
+++ src/app/order/submit/components/YourOrder.tsx
@@ -2,15 +2,11 @@
 import { ListDivider } from "@/components";
 import { useOrder } from "../../contexts/order_context/OrderContext";
 import DiscountCode from "./discount_code/DiscountCode";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { cartItems } from "../../types";
 
 const YourOrder = () => {
   const [discountItems, setDiscountItems] = useState<cartItems | null>(null);
-
-  useEffect(() => {
-    console.log(discountItems);
-  }, [discountItems]);
 
   return (
     <div className="flex flex-col">
@@ -25,30 +21,32 @@ const YourOrder = () => {
 
 export default YourOrder;
 
-type OrderListProps = {
-  discountItems: cartItems | null;
-};
-
 const OrderList = ({ discountItems }: OrderListProps) => {
   const { cartItems } = useOrder();
 
   return (
     <div className="flex w-full flex-col">
-      {discountItems
-        ? null
-        : cartItems.items.map((item, index) => (
-            <>
-              <div key={item.item.name} className="flex justify-between p-1">
-                <span className="w-1/2">{item.item.name}</span>
-                <span className="w-[3ch]">{item.count}</span>
-                <span>{`$${item.item.price}`}</span>
-              </div>
-              <ListDivider show={index !== cartItems.items.length - 1} />
-            </>
-          ))}
+      <Items items={discountItems ? discountItems : cartItems} />
       <div className="self-end pt-4">
         <p className="text-lg font-semibold">{`Total: $${cartItems.total}`}</p>
       </div>
     </div>
   );
+};
+
+const Items = ({ items }: { items: cartItems }) => {
+  return items.items.map((item, index) => (
+    <>
+      <div key={item.item.name} className="flex justify-between p-1">
+        <span className="w-1/2">{item.item.name}</span>
+        <span className="w-[3ch]">{item.count}</span>
+        <span>{`$${item.item.price}`}</span>
+      </div>
+      <ListDivider show={index !== items.items.length - 1} />
+    </>
+  ));
+};
+
+type OrderListProps = {
+  discountItems: cartItems | null;
 };
