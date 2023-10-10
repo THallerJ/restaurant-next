6cc5641c5da9124d0c1b0341f3bd4dc7b5e268d8
Thomas Haller <hallert60@gmail.com>
2023-10-09 20:36:27 -0500

    renamed breadcrumbs to bread_crumbs

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,7 @@ import Divider from "./Divider";
 import DailySpecialModal from "./DailySpecialModal";
 import { imageType } from "../types";
 import InputList from "./input_list/InputList";
-import Breadcrumbs from "./Breadcrumbs/Breadcrumbs";
+import Breadcrumbs from "./bread_crumbs/BreadCrumbs";
 import Hours from "./Hours";
 import AnimatedButton from "./AnimatedButton";
 import ListDivider from "./ListDivider";
