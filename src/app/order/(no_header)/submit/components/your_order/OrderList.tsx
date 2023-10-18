23940c3b53283b203a9f9c548708f3d80c4119e9
Thomas Haller <hallert60@gmail.com>
2023-10-17 20:20:48 -0500

    worked on adding discount line through on items

--- src/app/order/(no_header)/submit/components/your_order/OrderList.tsx
+++ src/app/order/(no_header)/submit/components/your_order/OrderList.tsx
@@ -1,18 +1,15 @@
 import { useOrder } from "@/app/order/contexts/order_context/OrderContext";
 import { useYourOrder } from "../../contexts/YourOrderContext";
-import { cartItems } from "@/app/order/types";
 import { ListDivider } from "@/components";
 import { useRouter } from "next/navigation";
 import { Add } from "@/assets";
 
 const OrderList = () => {
-  const { cartItems } = useOrder();
-  const { discountItems } = useYourOrder();
   const router = useRouter();
 
   return (
     <div className="flex w-full flex-col">
-      <Items items={discountItems ? discountItems : cartItems} />
+      <Items />
       <button
         className="flex items-center justify-center gap-2 rounded-b-lg 
           border-x-2 border-b-2 border-dark/25 p-3 shadow-sm"
@@ -29,17 +26,27 @@ const OrderList = () => {
 
 export default OrderList;
 
-const Items = ({ items }: { items: cartItems }) => {
+const Items = () => {
+  const { cartItems } = useOrder();
+  const { discountItems } = useYourOrder();
+
   return (
     <div className="rounded-t-lg border-2 border-dark/25 p-2 shadow-sm">
-      {items.items.map((item, index) => (
+      {cartItems.items.map((item, index) => (
         <>
           <div key={item.item.name} className="flex justify-between p-2">
             <span className="w-1/2">{item.item.name}</span>
             <span className="w-[3ch]">{item.count}</span>
-            <span>{`$${item.item.price.toFixed(2)}`}</span>
+            <div>
+              {discountItems ? <s>{item.item.price.toFixed(2)}</s> : null}
+              <span>{`$${
+                discountItems
+                  ? discountItems.items[index].item.price.toFixed(2)
+                  : item.item.price.toFixed(2)
+              }`}</span>
+            </div>
           </div>
-          <ListDivider show={index !== items.items.length - 1} />
+          <ListDivider show={index !== cartItems.items.length - 1} />
         </>
       ))}
     </div>
