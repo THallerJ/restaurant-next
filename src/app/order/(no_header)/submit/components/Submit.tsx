1a70c32b88b4494911f744db2f092292684c4cc2
Thomas Haller <hallert60@gmail.com>
2023-10-23 17:22:03 -0500

    made progress submitting orders

--- src/app/order/(no_header)/submit/components/Submit.tsx
+++ src/app/order/(no_header)/submit/components/Submit.tsx
@@ -2,6 +2,7 @@
 import YourOrder from "./your_order/YourOrder";
 import YourInformation from "./YourInformation";
 import { useOrder } from "../../../contexts/order_context/OrderContext";
+import { useState } from "react";
 
 const Submit = ({ children }: { children: React.ReactNode }) => {
   const { cartItems } = useOrder();
@@ -15,16 +16,27 @@ const Submit = ({ children }: { children: React.ReactNode }) => {
 
 export default Submit;
 
-const OrderForm = () => (
-  <form
-    onSubmit={(e) => e.preventDefault()}
-    className="flex w-full flex-col gap-8
+const OrderForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const { cartDispatch } = useOrder();
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+    cartDispatch({ type: "clear" });
+  };
+
+  return !submitted ? (
+    <form
+      onSubmit={onSubmit}
+      className="flex w-full flex-col gap-8
       rounded-lg border-4 border-dark bg-white p-4"
-  >
-    <YourOrder />
-    <YourInformation />
-    <button className="btn self-end" type="submit">
-      Finish order
-    </button>
-  </form>
-);
+    >
+      <YourOrder />
+      <YourInformation />
+      <button className="btn self-end" type="submit">
+        Finish order
+      </button>
+    </form>
+  ) : null;
+};
