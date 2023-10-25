8470ad72a138a0ee337c436bc1c56a6163a2b031
Thomas Haller <hallert60@gmail.com>
2023-10-24 20:10:25 -0500

    moved Submit to page

--- src/app/order/(no_header)/submit/page.tsx
+++ src/app/order/(no_header)/submit/page.tsx
@@ -1,15 +1,63 @@
-import Submit from "./components/Submit";
+"use client";
+import YourOrder from "./components/your_order/YourOrder";
+import YourInformation from "./components/YourInformation";
+import { useOrder } from "../../contexts/order_context/OrderContext";
+import { useState, useEffect } from "react";
 
-const page = () => {
+const SubmitPage = () => {
+  const { cartItems } = useOrder();
+  const [submittedFlag, setSubmittedFlag] = useState(false);
+
+  useEffect(() => {
+    if (cartItems && cartItems.count === 0) setSubmittedFlag(true);
+  }, [cartItems]);
+
   return (
-    <Submit>
-      <EmptyOrder />
-    </Submit>
+    <div className="flex h-full w-full flex-1 items-center justify-center">
+      {true ? (
+        <OrderForm
+          submittedFlag={submittedFlag}
+          setSubmittedFlag={setSubmittedFlag}
+        />
+      ) : (
+        <h1>hello</h1>
+      )}
+    </div>
   );
 };
 
-export default page;
+export default SubmitPage;
 
-const EmptyOrder = () => (
-  <p className="heading-sm">You have nothing in your cart</p>
-);
+type OrderFormProps = {
+  submittedFlag: boolean;
+  setSubmittedFlag: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const OrderForm = ({ submittedFlag, setSubmittedFlag }: OrderFormProps) => {
+  const { cartDispatch } = useOrder();
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmittedFlag(true);
+  };
+
+  useEffect(() => {
+    if (submittedFlag) cartDispatch({ type: "clear" });
+  }, [submittedFlag, cartDispatch]);
+
+  return true ? (
+    <form
+      onSubmit={onSubmit}
+      className="flex w-full flex-col gap-8
+      rounded-lg border-4 border-dark bg-white p-4"
+    >
+      <YourOrder />
+      <YourInformation />
+      <button className="btn self-end" type="submit">
+        Finish order
+      </button>
+    </form>
+  ) : (
+    <h1>none</h1>
+  );
+};
